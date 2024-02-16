da2bfbffa668a6be4adaa28267e085d585d6e591
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2024-02-16 13:18:54 +0600

    Created routes: POST /users, POST /users/sessions. Create middleware/auth.ts

--- todo-api/models/User.ts
+++ todo-api/models/User.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 import { UserFields, UserMethods, UserModel } from '../types';
+import { randomUUID } from 'node:crypto';
 
+const SALT_WORK_FACTOR = 10;
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema<UserFields, UserModel, UserMethods>({
@@ -16,6 +19,29 @@ const UserSchema = new Schema<UserFields, UserModel, UserMethods>({
   token: {
     type: String,
     required: true,
+  },
+});
+
+UserSchema.methods.checkPassword = function (password: string) {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateToken = function () {
+  this.token = randomUUID();
+};
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.set('toJSON', {
+  transform: (_doc, ret) => {
+    delete ret.password;
+    return ret;
   },
 });
 
---
+++ todo-api/routers/users.ts
@@ -0,0 +1,59 @@
+import { Router } from 'express';
+import User from '../models/User';
+import mongoose from 'mongoose';
+
+const userRouter = Router();
+
+userRouter.get('/', async (_req, res, next) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (e) {
+    next(e);
+  }
+});
+
+userRouter.post('/', async (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    user.generateToken();
+    await user.save();
+
+    return res.send(user);
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(422).send(e);
+    }
+
+    next(e);
+  }
+});
+
+userRouter.post('/sessions', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) {
+      return res.status(422).send({ error: 'Invalid credentials' });
+    }
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if (!isMatch) {
+      return res.status(422).send({ error: 'Invalid credentials' });
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return res.send({ user });
+  } catch (e) {
+    next(e);
+  }
+});
+
+export default userRouter;
